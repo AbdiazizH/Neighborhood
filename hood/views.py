997cecbdf2088923e5cef01336dd4098b433600b
AbdiazizH <a.aziz69890@gmail.com>
2021-07-29 01:16:19 +0300

    update veiws

--- hood/views.py
+++ hood/views.py
@@ -15,7 +15,7 @@ def index(request):
 def signup(request):  
     if request.method == 'POST':
         form = SignupForm(request.POST)  
-        if form.is_valid():
+        if form.is_valid(): 
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
