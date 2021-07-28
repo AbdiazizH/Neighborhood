6edabe1072f6c2d4891b409c8f03c630b09f0c14
AbdiazizH <a.aziz69890@gmail.com>
2021-07-29 01:21:07 +0300

    update urls.py

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
   
-urlpatterns = [ 
+urlpatterns = [   
     path('', views.index, name='index'),
     path('register/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
